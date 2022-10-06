e7655b2c93f4ae4c6c55f980a11c20e60b0eae0b
Glitch (luminous-ludicrous-squid) <none>
2022-10-06 00:29:59 +0000

    💓🏓 Checkpoint ./server.js:2108645/1820 ./package.json:2108645/5522

---
+++ server.js
@@ -0,0 +1,54 @@
+/**
+ * This is the main Node.js server script for your project
+ * Check out the two endpoints this back-end API provides in fastify.get and fastify.post below
+ */
+
+import Replicate from './replicate.js'
+
+const token = process.env['REP_API']
+
+const replicate = new Replicate({token: token});
+
+// If you set the REPLICATE_API_TOKEN environment variable, you do not need to provide a token to the constructor.
+// const replicate = new Replicate();
+
+import Fastify from 'fastify'
+const fastify = Fastify({
+  logger: true
+})
+
+fastify.route({
+  method: 'GET',
+  url: '/',
+  schema: {
+    // request needs to have a querystring with a `name` parameter
+    querystring: {
+      name: { type: 'string' }
+    }
+  },
+  // this function is executed for every request before the handler is executed
+  preHandler: async (request, reply) => {
+    // E.g. check authentication
+  },
+  handler: async (request, reply) => {
+    
+    const helloWorldModel = await replicate.models.get('replicate/hello-world');
+    console.log(helloWorldModel)
+    const helloWorldPrediction = await helloWorldModel.predict({ text: "test"});
+    console.log(helloWorldPrediction);
+    return { hello: helloWorldPrediction }
+  }
+})
+
+// Run the server and report out to the logs
+fastify.listen(
+  { port: process.env.PORT, host: "0.0.0.0" },
+  function (err, address) {
+    if (err) {
+      fastify.log.error(err);
+      process.exit(1);
+    }
+    console.log(`Your app is listening on ${address}`);
+    fastify.log.info(`server listening on ${address}`);
+  }
+);
